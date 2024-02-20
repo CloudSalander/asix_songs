d044b70d793e42e535fab7ea900140ffbfd1718f
Cloud <ruben.as532@gmail.com>
2024-02-20 18:10:08 +0100

    Created Playlist Class

---
+++ classes/Playlist.py
@@ -0,0 +1,7 @@
+class Playlist:
+    def __init__(self,title,songs = []):
+        self.title = title
+        self.songs = songs
+    
+    
+
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from classes.Song import *
+from classes.Playlist import *
 
 song1 = Song("Girl of the year","Beach House",3,51,Genre.DREAM_POP) 
 song2 = Song("Silver Soul","Beach Hosue",4,58,Genre.DREAM_POP)
 song3 = Song("La carretera nocturna","Triángulo de amor bizarro",3,24,Genre.INDIE)
 song4 = Song("Elevator Song","Keaton Henson",3,50,Genre.PIANO)
 
-song2.play()
+pl = Playlist("This is Beach House!",[song1,song2])
+print(pl)
